ad8440461a5570e5ba6bd338519e4d6228749dfc
dhami <rdhami425@gmail.com>
2012-02-17 22:40:40 +0545

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ MalaiNabirseu::Application.routes.draw do
 
   get "pages/donate"
 
-  get "pages/home"
+  get "pages/index"
 
   get "pages/new"
 
@@ -76,7 +76,7 @@ MalaiNabirseu::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'pages#home'
+   root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
