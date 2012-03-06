0bcb6e327db8747d2cfee3bd192978e097d062e3
revol-tech <revolt.techno@gmail.com>
2012-03-06 15:07:37 +0545

    slider complete

---
+++ app/assets/javascripts/resizer.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+   var dim = getDim();
+
+   setDim(dim,$('.anyClass'));
+   //$('.anyClass').find('.resize').width((dim['w']*2)).height(dim['h'])
+   setDim(dim,$('.anyClass').find('li'));
+   setDim(dim,$('.anyClass').find('img'));
+
+   $(window).resize(function(){
+
+    dim = getDim();
+
+    setDim(dim,$('.anyClass'));
+    //$('.anyClass').find('.resize').width((dim['w']*2)).height(dim['h'])
+    setDim(dim,$('.anyClass').find('li'));
+    setDim(dim,$('.anyClass').find('img'));
+   })
+
+
+   function getDim(){
+    var dim=[];
+    dim['w'] = $(window).width();
+    dim['h'] = $(window).height();
+    dim['h'] = 0.4*dim['w'];
+    return dim;
+   };
+
+   function setDim(dim,obj){
+    obj.width(dim['w']).height(dim['h']);
+   };
+})
+
+$(function(){
+  $(".anyClass").jCarouselLite({
+	  auto:true,
+  	speed:3000,
+  	visible:1,
+  	resizeable:true,
+  	showInfo:true,
+  	info_on_hover:false
+  });
+})
+
+$(function(){
+  $(".anyClass2").jCarouselLite({
+	    auto:true,
+	    speed:800,
+	    visible:5,
+	    showInfo:false,
+	    info_on_hover:true
+  });
+
+})
