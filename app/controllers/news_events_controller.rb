cd37731806fe45be6df14fc90d6ac532c7f8c195
ramesh <rdhami425@gmail.com>
2012-03-15 20:04:29 +0545

    news and events page

--- app/controllers/news_events_controller.rb
+++ app/controllers/news_events_controller.rb
@@ -1,8 +1,12 @@
 class NewsEventsController < ApplicationController
   def news_details
+		@children = ChildrenSlider.all()
+		@news = News.all()
   end
 
   def events_details
+		@children = ChildrenSlider.all()
+		@news = News.all()
   end
 
   def news
