8a37ef5480f47b896fcdf8b84d3b0d513b3b5987
kuber <kuberaaganja@gmail.com>
2012-04-19 10:34:51 +0545

    pagination fixed dynamically #10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,25 +5,58 @@ class PagesController < ApplicationController
      	 @howtohelp = HowToHelp.all()
     @children = ChildrenSlider.all()
     @banner=Banner.all()
+    @news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
 
+  end
+
+	def news_paginator
+     @news = News.order("news_desc").page(params[:page]).per(2)
+     respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def about_us
 		@children = ChildrenSlider.all()
+		@news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
   end
 
   def get_involved
 		@children = ChildrenSlider.all()
+		@news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
 
   end
 
   def events
 		@children = ChildrenSlider.all()
-    @news = News.order("news_desc").page(params[:page]).per(2)
+    @news_top =News.all()
+    @news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
   end
 
   def children
 		@children = ChildrenSlider.all()
+		@news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
 
   end
 
@@ -32,26 +65,33 @@ class PagesController < ApplicationController
 
   def what_we_do
 			@children = ChildrenSlider.all()
+			@news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
 
   end
 
   def how_to_help
   end
 
-  def donate
-		@children = ChildrenSlider.all()
 
-  end
 
 	 def site_map
   end
 
 	 def contact
   end
-	
+
 	def donate
 		@children = ChildrenSlider.all()
 		@news = News.all()
+		@news	 = News.order("news_desc").page(params[:page]).per(2)
+    respond_to do |format|
+      format.html
+      format.js
+      end
   end
 
 
