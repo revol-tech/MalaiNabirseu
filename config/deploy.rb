ee278521e3725113dc9b76f085ef661b8799eb5a
revol-tech <revolt.techno@gmail.com>
2012-06-12 20:51:34 +0545

    test julfi

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,15 @@
 default_run_options[:pty] = true
-ssh_options[:port] = 2020
-set :application, "malainabirseu"
+require "bundler/capistrano"
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+# Load RVM's capistrano plugin.
+require "rvm/capistrano"
+
+set :application, "MalaiNabirseu"
 set :repository,  "git@github.com:revol-tech/MalaiNabirseu.git"
 set :use_sudo, false  #permission conflict resolve
 set :scm, :git
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :deploy_to, "/home/deploy/www/malainebirseu.com.np"
@@ -12,30 +18,28 @@ role :web, "malainebirseu.com.np"                          # Your HTTP server, A
 role :app, "malainebirseu.com.np"                          # This may be the same as your `Web` server
 role :db,  "malainebirseu.com.np", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
-
 set :user, "deploy"
 set :scm_username, 'revol-tech'
 set :bundle_gemfile,  "Gemfile"
   set :bundle_dir,""
   set :bundle_flags,""
   set :bundle_without,      [:development, :test]
-require "bundler/capistrano"
 
-# Add RVM's lib directory to the load path.
+ssh_options[:forward_agent] = true
+ssh_options[:port] = 2020
 
 
-# Load RVM's capistrano plugin.
-require "rvm/capistrano"
+
 
 set :rvm_ruby_string, '1.9.3'
 #set :rvm_type, :user  # Don't use system-wide RVM
 #load 'deploy/assets'
 
-#db migrate
-after "deploy", "deploy:migrate"
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
 # If you are using Passenger mod_rails uncomment this:
  namespace :assets do
   desc "Compress assets in a local file"
@@ -60,7 +64,6 @@ after "deploy", "deploy:migrate"
     end
 end
 
-# If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
@@ -68,6 +71,7 @@ end
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
 before "deploy:update_code", "assets:compress_assets"
 after "deploy:update_code", "assets:upload_assets"
-after :deploy, 'deploy:cleanup','deploy:migrate', 'deploy:restart'
+after :deploy, 'deploy:cleanup','deploy:restart'
